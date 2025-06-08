702017bec354056547915d508655ef29e44dfcb6
karensandovals <ns590749@gmail.com>
2025-06-08 16:11:36 -0500

    Inyeccion de dependencias financiera

--- servicio financiera/src/main/java/co/edu/unicauca/distribuidos/core/modelos/Deuda.java
+++ servicio financiera/src/main/java/co/edu/unicauca/distribuidos/core/modelos/Deuda.java
@@ -1,7 +1,13 @@
 package co.edu.unicauca.distribuidos.core.modelos;
 
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor 
+@NoArgsConstructor  
 public class Deuda {
     private String codigoEstudiante;
     private double monto;
@@ -9,51 +15,4 @@ public class Deuda {
     private LocalDate fechaGeneracion;
     private LocalDate fechaLimite;
     private String estado; // pendiente, pagada, en mora
-    
-    public Deuda(String codigoEstudiante, double monto, String motivo, LocalDate fechaGeneracion, LocalDate fechaLimite,
-            String estado) {
-        this.codigoEstudiante = codigoEstudiante;
-        this.monto = monto;
-        this.motivo = motivo;
-        this.fechaGeneracion = fechaGeneracion;
-        this.fechaLimite = fechaLimite;
-        this.estado = estado;
-    }
-    public String getCodigoEstudiante() {
-        return codigoEstudiante;
-    }
-    public void setCodigoEstudiante(String codigoEstudiante) {
-        this.codigoEstudiante = codigoEstudiante;
-    }
-    public double getMonto() {
-        return monto;
-    }
-    public void setMonto(double monto) {
-        this.monto = monto;
-    }
-    public String getMotivo() {
-        return motivo;
-    }
-    public void setMotivo(String motivo) {
-        this.motivo = motivo;
-    }
-    public LocalDate getFechaGeneracion() {
-        return fechaGeneracion;
-    }
-    public void setFechaGeneracion(LocalDate fechaGeneracion) {
-        this.fechaGeneracion = fechaGeneracion;
-    }
-    public LocalDate getFechaLimite() {
-        return fechaLimite;
-    }
-    public void setFechaLimite(LocalDate fechaLimite) {
-        this.fechaLimite = fechaLimite;
-    }
-    public String getEstado() {
-        return estado;
-    }
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
 }
